595adf5552c6b4c52d3839fc1fc371b0b3395d28
Rolf Kreibaum <rolf.kreibaum@posteo.de>
2022-01-08 00:12:23 +0100

    Add decoders.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # rust_elm_typegen
+
 Generate elm types from rust types
+
+## Other Peoples Work
+
+The [json2elm](https://korban.net/elm/json2elm/) tool from Alex Korban has been
+great for quickly seeing how a decoder/encoder should look in elm.
--- src/main.rs
+++ src/main.rs
@@ -26,6 +26,14 @@ impl ElmType {
             ElmType::List(t) => format!("Json.Decode.list ({})", t.decoder_ref()),
         }
     }
+
+    fn encoder_ref(&self) -> String {
+        match self {
+            ElmType::Int => "Json.Encode.int".to_string(),
+            ElmType::String => "Json.Encode.string".to_string(),
+            ElmType::List(t) => format!("Json.Encode.list ({})", t.encoder_ref()),
+        }
+    }
 }
 
 // TODO: Handle snake_case -> camelCase conversion
@@ -93,16 +101,64 @@ impl ElmStruct {
         }
         output
     }
+
+    fn encoder_ref(&self) -> String {
+        format!("encode{}", self.name.0)
+    }
+
+    fn encoder_def(&self) -> String {
+        // Outputs something like:
+        // encodePerson : Person -> Json.Encode.Value
+        // encodePerson person =
+        //     Json.Encode.object
+        //         [ ( "age", Json.Encode.int person.age )
+        //         , ( "surname", Json.Encode.string person.surname )
+        //         ]
+        let mut output = format!(
+            "encode{} : {} -> Json.Encode.Value\n",
+            self.name.0, self.name.0
+        );
+        let this = self.name.0.to_lowercase();
+        output.push_str(format!("encode{} {} =\n", self.name.0, this).as_str());
+        output.push_str("    Json.Encode.object\n");
+        let mut is_first = true;
+        for (field, ty) in &self.fields {
+            if is_first {
+                output.push_str("        [ ");
+                is_first = false;
+            } else {
+                output.push_str("        , ");
+            }
+            output.push_str(
+                format!(
+                    "( \"{}\", {} {}.{} )\n",
+                    field.0,
+                    ty.encoder_ref(),
+                    this,
+                    field.0
+                )
+                .as_str(),
+            );
+        }
+        output.push_str("        ]\n");
+        output
+    }
 }
 
 fn main() {
-    println!("Hello, world!");
-    let ty = ElmType::List(Box::new(ElmType::Int));
-    assert_eq!(ty.type_ref(), "List (Int)");
-    assert_eq!(ty.decoder_ref(), "Json.Decode.list (Json.Decode.int)");
-    let ty = ElmType::String;
-    assert_eq!(ty.type_ref(), "String");
-    assert_eq!(ty.decoder_ref(), "Json.Decode.string");
+    let ty = ElmStruct {
+        name: Identifier("Person".to_string()),
+        fields: vec![
+            (Identifier("age".to_string()), ElmType::Int),
+            (Identifier("surname".to_string()), ElmType::String),
+        ],
+    };
+    println!("{}", ty.type_ref());
+    println!("{}", ty.type_def());
+    println!("{}", ty.decoder_ref());
+    println!("{}", ty.decoder_def());
+    println!("{}", ty.encoder_ref());
+    println!("{}", ty.encoder_def());
 }
 
 // Test module
@@ -117,18 +173,25 @@ mod tests {
         let ty = ElmType::Int;
         assert_eq!(ty.type_ref(), "Int");
         assert_eq!(ty.decoder_ref(), "Json.Decode.int");
+        assert_eq!(ty.encoder_ref(), "Json.Encode.int");
         let ty = ElmType::List(Box::new(ElmType::Int));
         assert_eq!(ty.type_ref(), "List (Int)");
         assert_eq!(ty.decoder_ref(), "Json.Decode.list (Json.Decode.int)");
+        assert_eq!(ty.encoder_ref(), "Json.Encode.list (Json.Encode.int)");
         let ty = ElmType::List(Box::new(ElmType::List(Box::new(ElmType::Int))));
         assert_eq!(ty.type_ref(), "List (List (Int))");
         assert_eq!(
             ty.decoder_ref(),
             "Json.Decode.list (Json.Decode.list (Json.Decode.int))"
         );
+        assert_eq!(
+            ty.encoder_ref(),
+            "Json.Encode.list (Json.Encode.list (Json.Encode.int))"
+        );
         let ty = ElmType::String;
         assert_eq!(ty.type_ref(), "String");
         assert_eq!(ty.decoder_ref(), "Json.Decode.string");
+        assert_eq!(ty.encoder_ref(), "Json.Encode.string");
     }
 
     #[test]
@@ -154,6 +217,19 @@ mod tests {
                     Json.Decode.succeed Person
                         |> Json.Decode.Pipeline.required \"age\" Json.Decode.int
                         |> Json.Decode.Pipeline.required \"surname\" Json.Decode.string
+                "
+            }
+        );
+        assert_eq!(ty.encoder_ref(), "encodePerson");
+        assert_eq!(
+            ty.encoder_def(),
+            indoc! {"
+                encodePerson : Person -> Json.Encode.Value
+                encodePerson person =
+                    Json.Encode.object
+                        [ ( \"age\", Json.Encode.int person.age )
+                        , ( \"surname\", Json.Encode.string person.surname )
+                        ]
                 "
             }
         );
